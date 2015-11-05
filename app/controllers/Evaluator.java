57379adef8aec2c2f555fdc35859e0de6b3bf4c5
joshuacamp <joshuacamp@github.com>
2015-11-04 21:21:14 -0600

    added several basic (some empty) classes to represent various components of the evaluation framework. Job class contains framework for processing one trial (including sending TextAnnotations to the solver and performing evaluations on the results)

---
+++ app/controllers/Evaluator.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
+import java.util.*;
+
+public interface Evaluator {
+
+	public Evaluation evaluate(List<TextAnnotation> correctAnnotations, List<TextAnnotation> solverAnnotations);
+
+}
