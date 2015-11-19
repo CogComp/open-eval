b4fcc5b131c42c6e6fd2fb5c8a28fefd05066946
joshuacamp <joshuacamp@github.com>
2015-11-19 15:30:58 -0600

    added  method

--- core/app/controllers/Core.java
+++ core/app/controllers/Core.java
@@ -1,9 +1,12 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import controllers.evaluators.Evaluator;
+import controllers.evaluators.SpanLabelingEvaluator;
+import controllers.evaluators.SpanSplittingEvaluator;
 import controllers.io.DatabaseCommunication;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import play.mvc.Controller;
@@ -12,12 +15,13 @@ import play.mvc.Result;
 public class Core extends Controller {
 	
 	public  Result startJob() {
+		/*
 		Map<String, String[]> params = request().body().asFormUrlEncoded();
 		List<TextAnnotation> instances = dbQuery(params.get("dataset")[0], params.get("problemType")[0]);
 		List<Evaluator> evaluators = getEvaluator(params.get("eval")[0]);
-		//Job newJob = new Job(params.get("url")[0], instances, newEval);
-		DummySolver dummySolver = new DummySolver();
-		Job job = new Job(dummySolver, instances, evaluators, Domain.TOY);
+		Job newJob = new Job(params.get("url")[0], instances, newEval);
+		*/
+		Job job = setUpToyJob();
 		job.sendAndReceiveRequestsFromSolver();
 		return ok("Received request");
 	}
@@ -29,5 +33,19 @@ public class Core extends Controller {
 	
 	private List<Evaluator> getEvaluator(String evalType){
 		return null;
+	}
+	
+	private Job setUpToyJob() {
+		DummySolver dummySolver = new DummySolver();
+		List<Evaluator> evaluators = new ArrayList<>();
+		List<TextAnnotation> instances = dbQuery("toyDataset", "");
+		
+		// Create evaluators for the toy job
+		SpanLabelingEvaluator spanLabelingEvaluator = new SpanLabelingEvaluator();
+		SpanSplittingEvaluator spanSplittingEvaluator = new SpanSplittingEvaluator();
+		evaluators.add(spanSplittingEvaluator);
+		evaluators.add(spanLabelingEvaluator);
+		
+		return new Job(dummySolver, instances, evaluators, Domain.TOY);
 	}
 }
