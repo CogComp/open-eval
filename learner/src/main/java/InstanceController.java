26d0128daa82b160c58500bcc63ae420d5331eda
ryannk <ryannkelch@gmail.com>
2015-12-19 19:54:15 -0600

    Consolidated Instance controller code

--- learner/src/main/java/InstanceController.java
+++ learner/src/main/java/InstanceController.java
@@ -16,36 +16,21 @@ public class InstanceController implements RouterNanoHTTPD.UriResponder
     public Response post(RouterNanoHTTPD.UriResource uriResource, Map<String, String> map, NanoHTTPD.IHTTPSession session)
     {
         Annotator annotator = uriResource.initParameter(Annotator.class);
-        Map<String, String> files = new HashMap<String, String>();
-        String json;
-        try {
-            session.parseBody(files);
-        } catch (IOException ex) {
+        String body = null;
+        try{
+            body = readBody(session);
+        } catch (Exception ex){
             Response errorResponse = ResponseGenerator.generateErrorResponse(ex, "Error reading request");
             return errorResponse;
-        } catch (NanoHTTPD.ResponseException e)
-        {
-            Response errorResponse = ResponseGenerator.generateErrorResponse(e,"Error reading request");
-            return errorResponse;
         }
-        json = files.get("postData");
-        ResultStruct result = this.getInstance(annotator,json);
-        if (result.error){
-            return ResponseGenerator.generateResponse(result.errorText, Response.Status.INTERNAL_ERROR);
-        }
-        else {
-            return NanoHTTPD.newFixedLengthResponse(result.result);
-        }
-    }
 
-    public ResultStruct getInstance(Annotator annotator, String body){
         TextAnnotation textAnnotation;
         try
         {
             textAnnotation = SerializationHelper.deserializeFromJson(body);
         } catch (Exception e)
         {
-            return new ResultStruct(null,true,"There was an error parsing the body: " + e.toString());
+            return ResponseGenerator.generateErrorResponse(e, "There was an error parsing the body");
         }
 
         try
@@ -53,12 +38,19 @@ public class InstanceController implements RouterNanoHTTPD.UriResponder
             annotator.addView(textAnnotation);
         } catch (AnnotatorException e)
         {
-            String errorMessage = "There was an error adding the view to the instance: " + e.toString();
-            return new ResultStruct(null,true,errorMessage);
+            return  ResponseGenerator.generateErrorResponse(e, "There was an error adding the view to the instance");
         }
 
         String jsonAnnotation = SerializationHelper.serializeToJson(textAnnotation);
-        return new ResultStruct(jsonAnnotation,false,null);
+        return NanoHTTPD.newFixedLengthResponse(jsonAnnotation);
+    }
+
+    private String readBody(NanoHTTPD.IHTTPSession session) throws Exception{
+        Map<String, String> files = new HashMap<String, String>();
+        String body;
+        session.parseBody(files);
+        body = files.get("postData");
+        return body;
     }
 
     @Override
