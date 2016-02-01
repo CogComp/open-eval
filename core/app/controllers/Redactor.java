fab05fad1382a8e777862e99753a9dd44d16bdf1
joshuacamp <joshuacamp@github.com>
2016-01-31 21:20:42 -0600

    made the Redactor class more user friendly

--- core/app/controllers/Redactor.java
+++ core/app/controllers/Redactor.java
@@ -1,11 +1,24 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 
 public class Redactor {
 
+	/**
+	 * Calls removeAnnotations without the need for the user to create a singleton list.
+	 * @param textAnnotations
+	 * @param viewToKeep
+	 * @return
+	 */
+	public static List<TextAnnotation> removeAnnotations(List<TextAnnotation> textAnnotations, String viewToKeep) {
+		List<String> viewsToKeep = new ArrayList<>();
+		viewsToKeep.add(viewToKeep);
+		return removeAnnotations(textAnnotations, viewsToKeep);
+	}
 	/**
 	 * Static method to remove views from `TextAnnotation` objects.  Each task variant has a number of views
 	 * necessary for the learner to solve it.  All other views should be removed.
@@ -14,6 +27,10 @@ public class Redactor {
 	 * @return
 	 */
 	public static List<TextAnnotation> removeAnnotations(List<TextAnnotation> textAnnotations, List<String> viewsToKeep) {
+		if (viewsToKeep == null) {
+			viewsToKeep = Collections.EMPTY_LIST;
+		}
+		
 		for (TextAnnotation textAnnotation : textAnnotations) {
 			for (String viewName : textAnnotation.getAvailableViews()) {
 				if (!viewsToKeep.contains(viewName)) {
