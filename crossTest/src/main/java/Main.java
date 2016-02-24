ed2de3b6898ea3f9714271f14f90af213b998856
joshuacamp <joshua.c.camp@gmail.com>
2016-02-23 21:38:25 -0600

    undid erroneous auto-merge changes to crossTest

--- crossTest/src/main/java/Main.java
+++ crossTest/src/main/java/Main.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
 /**
  * Created by rnkelch on 12/12/2015.
  */
@@ -10,12 +7,6 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        Server server = new Server(5757, new SaulPosAnnotator());
-        fi.iki.elonen.util.ServerRunner.executeInstance(server);
-    }
-    
-    @Test
-    public void run() throws IOException {
         Server server = new Server(5757, new SaulPosAnnotator());
         fi.iki.elonen.util.ServerRunner.executeInstance(server);
     }
