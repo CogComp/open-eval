aa0f66289d9b03f9d9d3283d533895d6acbb95c7
Ryan Kelch <ryannkelch@gmail.com>
2016-04-28 21:17:38 -0500

    fixed extra slash problem

--- learner/src/main/java/edu/illinois/cs/cogcomp/openeval/learner/Server.java
+++ learner/src/main/java/edu/illinois/cs/cogcomp/openeval/learner/Server.java
@@ -59,7 +59,7 @@ public class Server extends RouterNanoHTTPD
     private void printListeningAddress() throws UnknownHostException {
         InetAddress address = getLocalAddress();
         int port  = this.getListeningPort();
-        System.out.println(String.format("Your learner endpoint is listening at address is http://%s:%d/", address.toString(), port));
+        System.out.println(String.format("Your learner endpoint is listening at http://%s:%d/", address.getHostAddress(), port));
     }
 
     private static InetAddress getLocalAddress(){
